e38626714aff9ff8eb8b1cd21c176c2b955acdf7
kohdice <kohdice@gmail.com>
2023-06-23 17:45:44 +0900

    Revert "Raises a mypy error"
    This reverts commit 0fed72d678092459e5de78b480ffa801f766c88f.

--- sample/main.py
+++ sample/main.py
@@ -1,8 +1,8 @@
 class Sample:
-    def sample_finc(self, x, y):
+    def sample_finc(self, x: int, y: int) -> int:
         return x + y
 
-    def sample_finc_v2(self, x, y):
+    def sample_finc_v2(self, x: int, y: int) -> int:
         if not isinstance(x, int):
             raise TypeError
         if not isinstance(y, int):
@@ -10,5 +10,7 @@ class Sample:
 
         return x + y
 
-    def sample_list_finc(self, sample_dict):
+    def sample_list_finc(
+        self, sample_dict: dict[str, str]
+    ) -> list[tuple[str, str]]:
         return [(k, v) for k, v in sample_dict.items()]
